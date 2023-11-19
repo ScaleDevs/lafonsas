a0b7223504d23f1bb470a7d2ed35b114afacf0a7
Marco Theo Butalid <scaledevs@gmail.com>
2023-11-19 14:18:52 +0800

    added shadcn ui and shadcn ui checkbox

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-    './src/layouts/**/*.{ts,tsx}',
-    './src/modules/**/*.{ts,tsx}',
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
     screens: {
+      xs: '400px',
       sm: '640px',
       md: '768px',
       lg: '1080px',
@@ -22,10 +19,54 @@ module.exports = {
       raleway: ['Raleway', 'sans-serif'],
       comfortaa: ['Comfortaa', 'cursive'],
     },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       colors: {
-        primary: '#15803d',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primarylight: '#16a34a',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       animationDuration: {
         1000: '1000ms',
@@ -35,7 +76,7 @@ module.exports = {
         5000: '5000ms',
       },
       animationDelay: {
-        00005: '0.5ms',
+        '00005': '0.5ms',
         1000: '1000ms',
         2000: '2000ms',
         3000: '3000ms',
@@ -51,12 +92,22 @@ module.exports = {
           '0%': { transform: 'translateX(200%)' },
           '100%': { transform: 'translateX(30%)' },
         },
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
       },
       animation: {
         slideIn: 'slideIn ease-in',
         fadeIn: 'fadeIn ease-in',
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require('tailwindcss-animation')],
+  plugins: [require('tailwindcss-animate'), require('tailwindcss-animation')],
 };
